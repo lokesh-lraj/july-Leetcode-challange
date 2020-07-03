34e0dbc716863edc58c880abe90d7577e1246e4a
LOKESH_RAJ <52743591+lokesh-lraj@users.noreply.github.com>
2020-07-03 14:04:15 +0530

    Create Prision_cells_after_n_days.py
    Leetcode day - 3 of july daily challenge solution in Python.

---
+++ Prision_cells_after_n_days.py
@@ -0,0 +1,62 @@
+"""
+There are 8 prison cells in a row, and each cell is either occupied or vacant.
+
+Each day, whether the cell is occupied or vacant changes according to the following rules:
+
+If a cell has two adjacent neighbors that are both occupied or both vacant, then the cell becomes occupied.
+Otherwise, it becomes vacant.
+(Note that because the prison is a row, the first and the last cells in the row can't have two adjacent neighbors.)
+
+We describe the current state of the prison in the following way: cells[i] == 1 if the i-th cell is occupied, else cells[i] == 0.
+
+Given the initial state of the prison, return the state of the prison after N days (and N such changes described above.)
+
+Example 1:
+
+Input: cells = [0,1,0,1,1,0,0,1], N = 7
+Output: [0,0,1,1,0,0,0,0]
+Explanation: 
+The following table summarizes the state of the prison on each day:
+Day 0: [0, 1, 0, 1, 1, 0, 0, 1]
+Day 1: [0, 1, 1, 0, 0, 0, 0, 0]
+Day 2: [0, 0, 0, 0, 1, 1, 1, 0]
+Day 3: [0, 1, 1, 0, 0, 1, 0, 0]
+Day 4: [0, 0, 0, 0, 0, 1, 0, 0]
+Day 5: [0, 1, 1, 1, 0, 1, 0, 0]
+Day 6: [0, 0, 1, 0, 1, 1, 0, 0]
+Day 7: [0, 0, 1, 1, 0, 0, 0, 0]
+
+Example 2:
+
+Input: cells = [1,0,0,1,0,0,1,0], N = 1000000000
+Output: [0,0,1,1,1,1,1,0]
+ 
+
+Note:
+cells.length == 8
+cells[i] is in {0, 1}
+1 <= N <= 10^9
+
+"""
+class Solution:
+    def prisonAfterNDays(self, cells: List[int], n: int) -> List[int]:
+        n %= 14
+        if n == 0:
+            n = 14
+        for j in range(1, n+1):
+        
+            ans = []
+            #first cell is always vaccent
+            ans.append(0)
+            
+            for i in range(1, 7):
+                ans.append(1 if cells[i-1] == cells[i+1] else 0)
+                
+            #last cell is also always vaccent
+            ans.append(0)
+            
+            #cells state after end of jth day
+            cells = ans
+            
+        return cells
+        
