a9cda05a138fb815b5917afb8db53d09474b5e43
LOKESH_RAJ <52743591+lokesh-lraj@users.noreply.github.com>
2020-07-05 13:33:57 +0530

    Create Hamming_distance.py
    Leetcode July daily challenge day - 5 of 30 days solution in python.

---
+++ Hamming_distance.py
@@ -0,0 +1,31 @@
+"""
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+Given two integers x and y, calculate the Hamming distance.
+
+Note:
+0 ≤ x, y < 231.
+
+Example:
+
+Input: x = 1, y = 4
+
+Output: 2
+
+Explanation:
+1   (0 0 0 1)
+4   (0 1 0 0)
+       ↑   ↑
+
+The above arrows point to positions where the corresponding bits are different.
+"""
+
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        ans = 0
+        num = x ^ y
+        s = str(bin(num))[2:]
+        for elem in s:
+            if elem == '1':
+                ans += 1
+        return ans
